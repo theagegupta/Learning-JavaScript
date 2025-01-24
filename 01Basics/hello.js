a31aed28f80325e940da1e0c37987e2bee25db3d
Ayush Gupta <ayushgupta11580@gmail.com>
2025-01-24 16:03:30 +0000

    2nd commit

--- 01Basics/hello.js
+++ 01Basics/hello.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const account_Id = 100;
+let accountEmail = 'hitesh@google.com';
+var password = "12345";
+let accountCity = "Jaipur";
+
+// account_Id = 2;
+console.log(account_Id);
+console.table([account_Id, accountEmail, accountCity, password]);
